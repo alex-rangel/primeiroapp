9e81394a701134d06a6c04044fd3bc4384bb494c
Alex Rangel <alexrangel90@gmail.com>
2024-01-24 05:47:33 -0300

    inclusao de icones na aplicaçao

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -8,17 +8,20 @@ import {
     Botao,
     Link
 } from './style'
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 
 const SignIn = () => {
 
   return <Box>
+    
             <Titulo>LOOKAPP</Titulo>
             <Texto>SignIn my account.</Texto>
             <Input placeholder="E-mail" placeholderTextColor="lightgray"/>
             <Input margin="40px" placeholder="Password" placeholderTextColor="lightgray"/>
             <Botao><Text>Sigin into my account</Text></Botao>  
             <Link onPress={()=>alert('teste')}>Create new account</Link>
+            <Icon name="star" size={50} color="black" />
           </Box>
 };
 
